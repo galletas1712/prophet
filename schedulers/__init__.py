8b491b2986e967a8d73839ca18f15cf31ca9f958
Schwinn Saeree <17022745+galletas1712@users.noreply.github.com>
2024-06-06 05:19:54 +0000

    Fix import

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -2,5 +2,5 @@ from schedulers.utils import build_scheduler
 from schedulers.fcfs import FCFSScheduler
 from schedulers.random import RandomScheduler
 from schedulers.srpt import SRPTScheduler
-from schedulers.mlfq import SkipJoinMLFQ_scheduler
+from schedulers.skip_join_mlfq import SkipJoinMLFQ_Scheduler
 from schedulers.preemptible_srpt import PreemptibleSRPT_Scheduler
