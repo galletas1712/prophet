7ac8e0ed99bb659f7f34fbea7ad7d03c12723572
Ubuntu <ubuntu@ip-172-31-47-72.us-west-2.compute.internal>
2024-06-06 19:16:12 +0000

    temp

--- schedulers/srpt.py
+++ schedulers/srpt.py
@@ -8,29 +8,44 @@ from schedulers.utils import register_scheduler
 # TODO: scoring method
 @register_scheduler("srpt")
 class SRPTScheduler:
-    def __init__(self, batch_size, scoring_method, **kwargs) -> None:
+    def __init__(self, batch_size, scoring_method, starvation_limit=None, **kwargs) -> None:
         super(SRPTScheduler, self).__init__()
         self.batch_size = batch_size
         assert scoring_method == 'prefill_length'
         self.scoring_method = scoring_method
         self.requests = SortedDict()
 
+        self.starvation_limit = starvation_limit
+        self.now = 0
+
     def add_request(self, request) -> Request:
         # NOTE: Using prompt token length for now (even for decode)
         # NOTE: Important the we don't modify prompt_tokens after prefill!
+
+        # storage: (prompt length, request_id) -> (request, added_time or last_batched_time)
         self.requests[(len(request.prompt_tokens),
-                       request.request_id)] = request
+                       request.request_id)] = (request, self.now)
 
     def schedule(self, stage: RequestStage) -> List[Request]:
+        self.now += 1
+        
         batch = []
-
-        for _, request in self.requests.items():
+        for _, (request, _) in self.requests.items():
             assert request.stage is not RequestStage.DONE
             if request.stage is not stage:
                 continue
-            batch.append(request)
+            batch.append(request) 
+            # no need to update last time as always complet
             if len(batch) == self.batch_size:
                 break
+        
+        print([r.request_id for r in batch])
+
+        for _, (request, added_time) in self.requests.items():
+            if self.now - added_time > self.starvation_limit:
+                del self.requests[(len(request.prompt_tokens), request.request_id)]
+                self.requests[(0, request.request_id)] = (request, self.now)
+                print(f"Promoted starved request {request.request_id}")
 
         return batch
 
